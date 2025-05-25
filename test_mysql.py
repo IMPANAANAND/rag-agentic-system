7b09f8dcb4c6e6e893a3553f0910180850249491
IMPANAANAND <109675852+IMPANAANAND@users.noreply.github.com>
2025-05-25 15:03:36 +0530

    Update test_mysql.py

--- test_mysql.py
+++ test_mysql.py
@@ -3,7 +3,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Impana@2410",  # Replace with your password
+    password="",  # Replace with your password
     database="federal_register_db"
 )
 print("Connected successfully!")
